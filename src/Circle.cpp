3e6a1761cab9151fe1d4dd11db0f6817598ab54b
Hunor <vadobahunor@gmail.com>
2026-05-15 17:31:49 +0200

    Circle.cpp

---
+++ src/Circle.cpp
@@ -0,0 +1,17 @@
+#include "Circle.hpp"
+#include <iostream>
+#include <cmath>
+
+Circle::Circle(const std::string& name, const Vector& C, const Vector& V) 
+    : Shape(name, C), R(C.distance(V)) {}
+
+double Circle::area() const { return M_PI*R*R; }
+double Circle::perimeter() const { return M_PI*R*2; }
+bool Circle::contains(const Vector& v) const {
+    double d = C.distance(v);
+    return d <= R;
+}
+void Circle::print() const {
+    std::cout << name << " (kör)\n\tKözéppont: " << C 
+        << "\n\tSugara: " << R;
+}
